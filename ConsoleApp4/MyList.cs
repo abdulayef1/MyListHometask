4ccd9c6c473114ff1c251b61f239cf41021260f5
abdulayef1 <7ka1cy@.code.edu.az>
2022-12-02 06:13:46 +0400

    task is created

---
+++ ConsoleApp4/MyList.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.ComponentModel.DataAnnotations;
+
+namespace ConsoleApp4;
+
+public class MyList<T> : IEnumerable, IComparable
+{
+    private static int count = 0;
+
+    public int Capacity { get; set; }
+    public int Count { get => count; }
+    T[] items;
+
+    public MyList()
+    {
+        Capacity = 0;
+    }
+    public MyList(int capacity)
+    {
+        Capacity = capacity;
+    }
+
+    public void Add(T item)
+    {
+        if (Capacity == 0)
+        {
+            Capacity = 2;
+            Array.Resize(ref items, Capacity);
+            items[0] = item;
+            count++;
+            return;
+        }
+        if (Capacity == Count)
+        {
+            Capacity = Capacity * 2;
+            Array.Resize(ref items, Capacity);
+            items[Capacity / 2] = item;
+            count++;
+            return;
+        }
+        //[1,1,1,1]
+        items[Count] = item;
+        count++;
+        return;
+    }
+
+    public IEnumerator GetEnumerator()
+    {
+        Array.Resize(ref items, Count);
+        return items.GetEnumerator();
+    }
+    public bool Exist(Predicate<T> match)
+    {
+        foreach (var item in items)
+        {
+            if (match(item))
+                return true;
+        }
+        return false;
+    }
+
+    public int CompareTo(object? obj)
+    {
+        throw new NotImplementedException();
+    }
+}
+
---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using ConsoleApp4;
+
+MyList<int> myList = new MyList<int>();
+
+myList.Add(12);
+myList.Add(1);
+myList.Add(1);
+myList.Add(1);
+myList.Add(12);
+myList.Add(15);
+myList.Add(33);
+myList.Add(3);
+myList.Add(7);
+
+Console.WriteLine(myList.Exist(item => item == 16));
+
+
+
+//Console.WriteLine(myList.Capacity);
+
+
+foreach (var item in myList)
+{
+    Console.WriteLine(item);
+}
+
+
+
+
